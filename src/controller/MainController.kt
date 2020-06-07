c236bac7930839501b00355221bfeaeebb53af13
WildDisk <ivan.zolotopup@gmail.com>
2020-06-07 15:08:48 +1000

    added api for add and get users

--- src/controller/MainController.kt
+++ src/controller/MainController.kt
@@ -3,11 +3,13 @@ package ru.wilddisk.controller
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.freemarker.FreeMarkerContent
+import io.ktor.request.receive
 import io.ktor.request.receiveParameters
 import io.ktor.response.respond
 import io.ktor.response.respondRedirect
 import io.ktor.routing.get
 import io.ktor.routing.post
+import io.ktor.routing.route
 import io.ktor.routing.routing
 import ru.wilddisk.model.User
 import ru.wilddisk.users
@@ -30,12 +32,23 @@ fun Application.hello() {
 }
 
 /**
- * returns json objects
+ * API
  */
 fun Application.json() {
     routing {
-        get("/json") {
-            call.respond(users)
+        route("/api/") {
+            get("users") {
+                call.respond(users)
+            }
+            get("user/{id}") {
+                val id = call.parameters["id"]?.toInt() ?: 0
+                call.respond(users[id])
+            }
+            post("user") {
+                val post = call.receive<User>()
+                users.add(User(post.name, post.email, LocalDateTime.now()))
+                call.respond(mapOf("ok" to true))
+            }
         }
     }
 }
